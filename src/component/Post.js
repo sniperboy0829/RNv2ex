cbd8a4ccf54a15b301b6657f7aa2485e30ecfee0
xinminghui <xinming.hui@essexlg.com>
2020-08-04 15:11:52 +0800

    enriched post style

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row',
     marginHorizontal: 20,
     marginTop: 30,
+    padding: 10,
     backgroundColor: '#009999',
     borderRadius: 5,
   },
+  content: {
+    flex: 1,
+    marginLeft: 10,
+    justifyContent: 'space-between',
+  },
   item: {
-    margin: 20,
     fontSize: 20,
+  },
+  signature: {
+    fontSize: 14,
+    marginTop: 10,
+  },
+  avtar: {
+    width: 70,
+    height: 70,
   },
 });
 
 export function Post({post, onClick}) {
+  const avatarURL = post.member.avatar_mini;
+  let actuallyAvatarURL = avatarURL;
+  const indexAvatarURL = avatarURL.indexOf('?m=');
+  if (indexAvatarURL !== -1) {
+    actuallyAvatarURL = avatarURL.substring(0, indexAvatarURL);
+  }
+  let signature = post.member.username + '    5分钟前';
   return (
-    <View style={styles.container}>
-      <Text style={styles.item} onPress={() => onClick(post.id)}>
-        {post.title}
-      </Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={() => onClick(post.id)}>
+      <Image style={styles.avtar} source={{uri: actuallyAvatarURL}} />
+      <View style={styles.content}>
+        <Text style={styles.item}>{post.title}</Text>
+        <Text style={styles.signature}>{signature}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
